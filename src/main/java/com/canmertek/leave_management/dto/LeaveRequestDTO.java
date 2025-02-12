baabbda24677e63e4584e63b78b82dc4cd45c353
canmrtk <mertek33@gmail.com>
2025-02-12 20:05:15 +0300

    güncelleme

--- src/main/java/com/canmertek/leave_management/dto/LeaveRequestDTO.java
+++ src/main/java/com/canmertek/leave_management/dto/LeaveRequestDTO.java
@@ -1,7 +1,14 @@
 package com.canmertek.leave_management.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
 public class LeaveRequestDTO {
+
+    @NotNull(message = "Çalışan ID boş olamaz!")
     private Long employeeId;
+
+    @Min(value = 1, message = "En az 1 gün izin almalısınız!")
     private int leaveDays;
 
     public LeaveRequestDTO() {}

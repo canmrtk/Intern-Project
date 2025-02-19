28b5950f101de541c22d356a607b0e4ce5c9a576
canmrtk <mertek33@gmail.com>
2025-02-19 23:07:03 +0300

    güncelleme 10

--- src/main/java/com/canmertek/leave_management/repository/LeaveRequestRepository.java
+++ src/main/java/com/canmertek/leave_management/repository/LeaveRequestRepository.java
@@ -1,5 +1,6 @@
 package com.canmertek.leave_management.repository;
 
+import com.canmertek.leave_management.model.Employee;
 import com.canmertek.leave_management.model.LeaveRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,4 +10,5 @@ import java.util.List;
 @Repository
 public interface LeaveRequestRepository extends JpaRepository<LeaveRequest, Long> {
     List<LeaveRequest> findByEmployeeId(Long employeeId);
+    boolean existsByEmployeeAndStatus(Employee employee, String status);
 }
